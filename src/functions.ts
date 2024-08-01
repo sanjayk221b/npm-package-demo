be3a8f96b91f7552bb93887144fde7792a18299e
Sanjay <sanjaykeloth321@gmail.com>
2024-08-01 11:11:51 +0530

    feat: implement sayHello function with TypeScript types

---
+++ src/functions.ts
@@ -0,0 +1,13 @@
+import { SayHelloProps } from "./types";
+
+export function sayHello({ firstName, lastName, age }: SayHelloProps) {
+  console.log("Hello");
+  console.log(`Your first name is ${firstName}`);
+
+  if (lastName) {
+    console.log(`Your first name is ${lastName}`);
+  }
+  if (age) {
+    console.log(`Your age is ${age}`);
+  }
+}
